ec18324c36099a2ecdaeb83ccd4aae9b5c666496
Xuesong <amorphobia@users.noreply.github.com>
2026-07-01 11:26:08 +0800

    ➕️ Add native Windows build

--- src/pyrime/__init__.py
+++ src/pyrime/__init__.py
@@ -2,6 +2,7 @@ r"""Provide ``__version__`` for
 `importlib.metadata.version() <https://docs.python.org/3/library/importlib.metadata.html#distribution-versions>`_.
 """
 
+import os, sys
 from dataclasses import dataclass
 from enum import Enum, auto
 
@@ -9,6 +10,11 @@ __version__ = "@PROJECT_VERSION@"
 # wrong rime.distribution_version will result in error
 if "@" in __version__:
     __version__ = "0.0.1"
+
+if sys.platform == "win32":
+    lib_dir = os.environ.get("LIBRIME_LIB_DIR")
+    if lib_dir and os.path.isdir(lib_dir):
+        os.add_dll_directory(lib_dir)
 
 
 class LogLevel(Enum):
